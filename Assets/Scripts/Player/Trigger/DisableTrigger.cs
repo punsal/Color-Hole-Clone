a6ffe827ce987b54a5e8841561c29707c2beada9
Mustafa Pamir Ünsal <pamir.unsal@gmail.com>
2020-06-16 16:22:07 +0300

    Player's disabling problem is fixed via UniRx observables.

--- Assets/Scripts/Player/Trigger/DisableTrigger.cs
+++ Assets/Scripts/Player/Trigger/DisableTrigger.cs
@@ -1,4 +1,7 @@
-﻿using Collectible;
+﻿using System;
+using Collectible;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using Utilities.Behaviour.Trigger;
 
@@ -6,6 +9,26 @@ namespace Player.Trigger
 {
     public class DisableTrigger : TriggerController
     {
+        private Collider playerCollider;
+
+        private void OnEnable()
+        {
+            if (playerCollider == null)
+            {
+                playerCollider = GetComponent<Collider>();
+            }
+
+            playerCollider.OnTriggerStayAsObservable()
+                .Select(other => tags.Contains(other.tag) ? other.gameObject : null)
+                .Subscribe(o =>
+                {
+                    if (o != null)
+                    {
+                        o.SetActive(false);
+                    }
+                });
+        }
+
         protected override void OnTriggerEnterAction(Collider other)
         {
             var collectibleItem = other.GetComponent<CollectibleItem>();
